fb9032ef410c00df827ef01eb1b7619bee71d925
MargelovN <margelovn@gmail.com>
2021-09-28 23:01:58 +0900

    ADDED pom.xml, src, .gitignore

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,15 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int sqrtNumber(int min, int max) {
+        int counter = 0;
+        int number;
+        for (int i = 10; i < 100; i++) {
+            number = i * i;
+            if ((number > min) && (number < max)) {
+                counter = counter + 1;
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,18 @@
+package ru.netology.sqr;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQRServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void shouldSqrtNumber() {
+    SQRService service = new SQRService();
+
+    int min = 200;
+    int max = 300;
+    int expected = 3;
+        int actual = service.sqrtNumber(min, max);
+                assertEquals(expected, actual);
+
+    }
+}
